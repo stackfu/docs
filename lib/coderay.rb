9f4c85d125120c1045dd5fb5db9b24b47cf5ea78
Felipe Coury <felipe.coury@gmail.com>
2010-10-15 16:03:25 -0300

    Initial commit

---
+++ lib/coderay.rb
@@ -0,0 +1,15 @@
+require 'coderay'
+
+def code(*args, &block)
+  buffer = eval('_erbout', block.binding)
+  
+  lang = args.first || :ruby
+  pos = buffer.length
+  block.call(*args)
+  data = buffer[pos..-1]
+  buffer[pos..-1] = codify(data, lang)
+end
+
+def codify(str, lang)
+  %{<pre><code class="#{lang}">#{CodeRay.scan(str, lang).html}</code></pre>}
+end
